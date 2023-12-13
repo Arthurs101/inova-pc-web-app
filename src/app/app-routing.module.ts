580dbe3b0e44732bc760a86b24a81b62834e9bf1
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2023-12-12 21:55:46 -0600

    navbar-created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './NotFound/NotFound.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'',redirectTo: '/login', pathMatch: 'full'},
+  {path:'main',component: MainDashboardComponent},
   {path:'**', component: NotFoundComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './NotFound/NotFound.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
       LoginComponent,
-      NotFoundComponent
+      NotFoundComponent,
+      MainDashboardComponent
    ],
   imports: [
     BrowserModule,
